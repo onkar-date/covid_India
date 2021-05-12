070a1b982255a1668efc40ceab21a16b3226fa42
onkar-date <onkar101197@gmail.com>
2021-05-12 18:23:13 +0530

    district removed

--- src/app/shared/constants/sidebar-menu.constant.ts
+++ src/app/shared/constants/sidebar-menu.constant.ts
@@ -11,12 +11,12 @@ export const SidebarMenuItem = [
     icon: 'fa fa-university',
     active: false
   },
-  {
-    name: 'District',
-    url: 'district',
-    icon: 'fa fa-building',
-    active: false
-  },
+  // {
+  //   name: 'District',
+  //   url: 'district',
+  //   icon: 'fa fa-building',
+  //   active: false
+  // },
   {
     name: 'Vaccine',
     url: 'vaccine',
